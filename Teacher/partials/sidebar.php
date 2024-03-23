e751f9b47772d40e8fb84cbd2077f58fbbf67790
Hangchamchi <21520822@gm.uit.edu.vn>
2024-03-23 16:55:05 +0700

    table giao vien bo mon

--- Teacher/partials/sidebar.php
+++ Teacher/partials/sidebar.php
@@ -6,14 +6,14 @@
         <span class="menu-title">Dashboard</span>
       </a>
     </li>
-    <li class="nav-item nav-category">Chức năng của giáo viên chủ nhiệm</li>
+    <li class="nav-item nav-category">Giáo viên chủ nhiệm</li>
     <li class="nav-item">
       <a class="nav-link"  href="index.php?action=DanhSachHocSinh" aria-expanded="false" aria-controls="ui-basic">
         <i class="menu-icon mdi mdi-floor-plan"></i>
         <span class="menu-title">Danh sách học sinh</span>
       </a>
     </li>
-    <li class="nav-item nav-category">Chức năng của giáo viên bộ môn</li>
+    <li class="nav-item nav-category">Giáo viên bộ môn</li>
     <li class="nav-item">
       <a class="nav-link" href="index.php?action=SuaDiem" aria-expanded="false" aria-controls="form-elements">
         <i class="menu-icon mdi mdi-card-text-outline"></i>
@@ -23,100 +23,46 @@
   </ul>
 </nav>
 
-<!-- 
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
+<!-- <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav">
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
+      <a class="nav-link" href="../../Teacher/pages/DashBoard.php">
         <i class="mdi mdi-grid-large menu-icon"></i>
         <span class="menu-title">Trang chủ</span>
       </a>
     </li>
-    <h4>TEST</h4>
 
-    <li class="nav-item nav-category">UI Elements</li>
+    <li class="nav-item nav-category">Giáo viên chủ nhiệm</li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+      <a class="nav-link" data-bs-toggle="collapse" href="../../Teacher/pages/GVCM/DanhSachHocSinhPage.php" aria-expanded="false" aria-controls="ui-basic">
         <i class="menu-icon mdi mdi-floor-plan"></i>
-        <span class="menu-title">UI Elements</span>
+        <span class="menu-title">Danh sách học sinh</span>
         <i class="menu-arrow"></i>
       </a>
-      <div class="collapse" id="ui-basic">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
-        </ul>
-      </div>
     </li>
-    <li class="nav-item nav-category">Forms and Datas</li>
+
+    <li class="nav-item nav-category">Giáo viên bộ môn</li>
     <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+      <a class="nav-link" data-bs-toggle="collapse" href="../../Teacher/pages/GVBM/SuaDiemPage.php" aria-expanded="false" aria-controls="form-elements">
         <i class="menu-icon mdi mdi-card-text-outline"></i>
-        <span class="menu-title">Form elements</span>
+        <span class="menu-title">Bảng điểm</span>
         <i class="menu-arrow"></i>
       </a>
-      <div class="collapse" id="form-elements">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
-        </ul>
-      </div>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
-        <i class="menu-icon mdi mdi-chart-line"></i>
-        <span class="menu-title">Charts</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="charts">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-        <i class="menu-icon mdi mdi-table"></i>
-        <span class="menu-title">Tables</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="tables">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-        <i class="menu-icon mdi mdi-layers-outline"></i>
-        <span class="menu-title">Icons</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="icons">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item nav-category">pages</li>
+
+    <li class="nav-item nav-category">Tài khoản</li>
     <li class="nav-item">
       <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
         <i class="menu-icon mdi mdi-account-circle-outline"></i>
-        <span class="menu-title">User Pages</span>
+        <span class="menu-title">Trang cá nhân</span>
         <i class="menu-arrow"></i>
       </a>
       <div class="collapse" id="auth">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Đăng xuất </a></li>
         </ul>
       </div>
     </li>
-    <li class="nav-item nav-category">help</li>
-    <li class="nav-item">
-      <a class="nav-link" href="http://bootstrapdash.com/demo/star-admin2-free/docs/documentation.html">
-        <i class="menu-icon mdi mdi-file-document"></i>
-        <span class="menu-title">Documentation</span>
-      </a>
-    </li>
+
   </ul>
 </nav> -->
