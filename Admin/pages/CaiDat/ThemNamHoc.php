95e539b746e881bfabf7aaf57184ed0622c32222
Nguyễn Thị Thúy Hằng <95228523+hanachamchi@users.noreply.github.com>
2024-05-22 15:11:26 +0700

    Add files via upload

--- Admin/pages/CaiDat/ThemNamHoc.php
+++ Admin/pages/CaiDat/ThemNamHoc.php
@@ -1,14 +1,35 @@
-<?php 
+<?php
 include "../../../Database/Config/config.php";
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $nam1 = $_POST['nam1'];
     $nam2 = $nam1 + 1;
 
-    $sql = "INSERT INTO NAMHOC (nam1, nam2) VALUES ('$nam1', '$nam2')";
+    $mysqli->begin_transaction();
 
-    $result =$mysqli->query($sql);
-    header("Location: http://localhost:3000/Admin/index.php?action=CaiDat");
-    exit();
+    try {
+        $sql = "INSERT INTO NAMHOC (nam1, nam2) VALUES ('$nam1', '$nam2')";
+        $mysqli->query($sql);
+
+        $maNamHoc = $mysqli->insert_id;
+
+        $sql = "SELECT MaLop FROM lop";
+        $result = $mysqli->query($sql);
+
+
+        while ($row = $result->fetch_assoc()) {
+            $maLop = $row['MaLop'];
+            $sqlInsertDanhSachLop = "INSERT INTO danhsachlop (MaNamHoc, MaLop, SiSo) VALUES ('$maNamHoc', '$maLop', 0)";
+            $mysqli->query($sqlInsertDanhSachLop);
+        }
+
+        $mysqli->commit();
+
+        header("Location: http://localhost:3000/Admin/index.php?action=CaiDat");
+        exit();
+    } catch (Exception $e) {
+        // Rollback the transaction if something went wrong
+        $mysqli->rollback();
+        echo "Error: " . $e->getMessage();
+    }
 }
-?>
