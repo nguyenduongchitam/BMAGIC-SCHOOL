349e2d117d143a3d83af9e915200c82eec2fad9a
UIT21522338 <118320206+UIT21522338@users.noreply.github.com>
2024-05-24 23:11:06 +0700

    avd

--- Admin/partials/sidebar.php
+++ Admin/partials/sidebar.php
@@ -2,7 +2,7 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav">
     <li class="nav-item">
-      <a class="nav-link" href="../../Admin/index.php">
+      <a class="nav-link" href="../../../BMAGIC-SCHOOL/Admin/index.php">
         <i class="mdi mdi-grid-large menu-icon"></i>
         <span class="menu-title">Trang chủ</span>
       </a>
