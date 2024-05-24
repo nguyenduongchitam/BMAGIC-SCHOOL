b17199f58a4219e1967f2b6efb53ae070c4b625d
wkZandy <21521107@gm.uit.edu.vn>
2024-05-24 22:56:01 +0700

    delete lop

--- Admin/pages/QuanLyLop/DeleteLop.php
+++ Admin/pages/QuanLyLop/DeleteLop.php
@@ -3,20 +3,36 @@ include "../../../Database/Config/config.php";
 
 if (isset($_GET['MaLop'])) {
     $MaLop = $_GET['MaLop'];
+    $flag = true;
 
-    $sql = $mysqli->prepare("DELETE FROM LOP WHERE MaLop = ?");
-    $sql->bind_param("s", $MaLop);
+    $sql1 = "SELECT MADSL FROM DANHSACHLOP WHERE MALOP = $MaLop";
+    $result1 = $mysqli->query($sql1);
 
-    try {
-        $sql->execute();
-        header("Location: /Admin/index.php?action=QuanLyLop");
-        exit();
-    } catch (mysqli_sql_exception $e) {
-        echo "<script>
-                alert('Không xóa được lớp học. Lớp học đang có học sinh.');
-                window.location.href='/Admin/index.php?action=QuanLyLop';
-              </script>";
+    while ($row1 = $result1->fetch_assoc()) {
+        $maDSL = $row1['MADSL'];
+        $sql2 = "SELECT COUNT(*) AS SUM FROM CHITIETDANHSACHLOP WHERE MADSL = $maDSL";
+        $result2 = $mysqli->query($sql2);
+        $row2 = $result2->fetch_assoc();
+
+        if ($row2['SUM'] > 0) {
+            $flag = false;
+            echo "<script>
+            alert('Không xóa được lớp. Lớp học đang có học sinh.');
+            window.location.href='http://localhost:3000/Admin/index.php?action=QuanLyLop';
+          </script>";
+            break;
+        }
     }
+    if ($flag == true) {
 
-    $stmt->close();
+
+        $sql3 = "DELETE FROM DANHSACHLOP WHERE MaLop = $MaLop";
+        $mysqli->query($sql3);
+
+        $sql4 = "DELETE FROM LOP WHERE MaLop = $MaLop";
+        $mysqli->query($sql4);
+        echo "<script>
+        alert('Xóa thành công')window.location.href='http://localhost:3000/Admin/index.php?action=QuanLyLop';
+        </script>";
+    }
 }
